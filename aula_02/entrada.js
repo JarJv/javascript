556559328f156329f6573f7c6880c8e539deaebe
JarJv <79714103+JarJv@users.noreply.github.com>
2021-10-27 14:50:01 -0300

    Add files via upload

---
+++ aula_02/entrada.js
@@ -0,0 +1,6 @@
+function pedirvalores(){
+    num = parseInt(prompt("Digite o primeiro número: "))
+    num2 = parseInt(prompt("Digite o segundo número: "))
+    soma = num + num2
+    subtracao = num - num2
+}
